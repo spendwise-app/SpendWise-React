80d56b0a8311027adf8162021bb1dfaf79df84b6
sreeraganil <sreeraganil2003@gmail.com>
2025-06-09 14:42:19 +0530

    stable version

--- src/pages/Friends/Friends.jsx
+++ src/pages/Friends/Friends.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import API from "../../config/axios";
 import BackHeader from "../../component/BackHeader/BackHeader";
